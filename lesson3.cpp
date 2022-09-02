cc56853b530b681da0a1c643b28cf2d4f1337cca
Afrina Parvin Moury <74785814+Afina-1696@users.noreply.github.com>
2022-09-02 09:33:11 +0600

    1ST COMMIT

--- lesson3.cpp
+++ lesson3.cpp
@@ -11,11 +11,11 @@ int main ()
 	cout << (a == b) <<endl ;
 	/*condition check operator ==, !=, <=, >=, <, >, &&, ||
 	&& operator means both conditions have to be true.
-	|| operator means if any condition is true the condition is true 
+	|| operator means if any condition is true the condition is true
 	Here output 1 means true and 0 mean false*/
 	cout << ((b>a) && (c>b)) << endl;
 	cout << ((b>a) || (c>b)) << endl;
-	
+
 	int n;
 	cin >> n ;
 	if (n%2 == 0){
@@ -24,8 +24,31 @@ int main ()
 	     cout << "Divisible by 3" << endl;}
 	  else {
 	      cout << "Odd" << endl;}
-	      
-	      
-	      
+
+	int x = 1;
+	while(x <= 10)
+	{
+		cout << x << endl;
+		x++;
+	}
+
+	for(int i=0; i <= 10; i++)
+	{
+		cout << i << endl;
+	}
+
+    //test case and double loop example
+    int z,t;
+    cin >> t;
+    for(int test=0; test<t; test++){
+    /**/
+    for(int v =1; v <= z; v++){
+        for(int j=1; j<=v; j++)//ay linetar mane hoilo
+        {
+
+        }
+    }
+    }
+
 	return 0;
 }
