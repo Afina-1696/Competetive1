aaaa3b73918a6b139f4f4d2eca68c534f8ef21b9
Afrina Parvin Moury <74785814+Afina-1696@users.noreply.github.com>
2022-09-02 15:08:16 +0600

    1st commit

--- lesson3.cpp
+++ lesson3.cpp
@@ -41,14 +41,55 @@ int main ()
     int z,t;
     cin >> t;
     for(int test=0; test<t; test++){
-    /**/
+    /*while(t--) ay loop o use kora jay
+    t jokhon 0 hoyw jabe loop off hoye jabe karon 0 mean false and condition dont match*/
+    cin >> z;
     for(int v =1; v <= z; v++){
-        for(int j=1; j<=v; j++)//ay linetar mane hoilo
+        for(int j=1; j<=v; j++)//ay linetar mane hoilo j loop ta v bar cholbe
         {
+            cout << "*" ;
 
         }
+        cout << endl;
     }
     }
+
+    //infinet loop
+    /*while() or while(1){
+    }
+
+    for(;;){
+        }
+        condition true hoy na tai infinetly cholte thakbe*/
+
+
+    //jump statement break and continue
+    for(int q=0; q<3; q++){
+        for(int p=0; p<10; p++){
+            if(p==8){
+                break;//last for loop orthat p loop er baire cole jabe
+                //continue; last for loop orthat p loop er upore chole jabe and loop abr ocholte thakbe
+            }
+            cout << p << endl;
+        }
+    }
+
+
+    cin >> t;
+    while(t--){
+        int u;
+        cin >> u;
+        int digit_sum = 0;
+        while(u > 0){
+            int last_digit = u%10;
+            cout << "last Digit = " << last_digit << endl;
+            digit_sum = digit_sum + last_digit;
+            u = u/10;
+            cout << "u number after minus = " << u << endl;
+        }
+        cout << "sum of digits = " << digit_sum << endl;
+    }
+
 
 	return 0;
 }
